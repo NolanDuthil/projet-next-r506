421d07fc950298bcb949254e65cf0d68337e2390
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2024-12-06 15:20:22 +0100

    test

--- app/dashboard/disponibility/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Disponibility() {
-
-    return (
-        <main className="flex min-h-screen flex-col p-6">
-            <p>Disponibilités des intervenants</p>
-        </main>
-    );
-}
